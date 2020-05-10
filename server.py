6731b4c4ac31595386270c1424ecb3187be5e20f
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-10 00:14:28 -0400

    + Added flask assets plus added common bp

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from flaskext.mysql import MySQL
 from flask_sqlalchemy import SQLAlchemy
+from flask_assets import Environment
+from mrc_bundles import bundles
 import secrets
-
 
 mysql = MySQL()
 db = SQLAlchemy()
-
 
 def create_server(config):
     app = Flask(__name__)
@@ -23,33 +23,24 @@ def create_server(config):
         # initialize extensions
         mysql.init_app(app)
         db = SQLAlchemy(app)
-        
         db.create_all()
-
-
-
-        from models import User
-        @app.route('/')
-        def index():
-            test = User(email='hello',firstname='hi',lastname='hi',password='hi')
-            db.session.add(test)
-            db.session.commit()
-            return 'Hello'
 
         #secret_key generation
         app.secret_key = secrets.token_urlsafe(256)
-        
+
+        assets = Environment(app)
+        assets.register(bundles)
 
         # if you reformat this code, the imports go up resulting in
         # circular importing which breaks the blueprint architecture
 
-        # from routes.common import routes as common_routes
+        from routes.common import routes as common_routes
         # from routes.user import routes as user_routes
         # from routes.admin import routes as admin_routes
         # from routes.api import routes as api_routes
         # from routes.errors.routes import page_not_found,logic_error
 
-        # app.register_blueprint(common_routes.common_bp)
+        app.register_blueprint(common_routes.common_bp)
         # app.register_blueprint(user_routes.user_bp, url_prefix="/nileuser")
         # app.register_blueprint(admin_routes.admin_bp, url_prefix="/admin")
         # app.register_blueprint(api_routes.api_bp,url_prefix="/api")
@@ -58,3 +49,5 @@ def create_server(config):
         # app.register_error_handler(500,logic_error)
 
     return app
+
+
