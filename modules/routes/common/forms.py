9cac120525b547b994653151e8e06e0026544370
ssyuen <samuel.s.yuen@gmail.com>
2020-05-10 19:26:31 -0400

    + LoginForm integrated

--- modules/routes/common/forms.py
+++ modules/routes/common/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import InputRequired,Length,AnyOf,Email
+from wtforms.validators import InputRequired,Length,AnyOf
 
 class LoginForm(FlaskForm):
-    email = EmailField("Enter Email",validators=[InputRequired(),Email()])
+    email = EmailField("Enter Email",validators=[InputRequired()])
     password = PasswordField('Enter Password',validators=[InputRequired()])
