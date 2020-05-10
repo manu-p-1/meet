bff189db03a4c5e2398175e74244e9f32f4a61d7
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-10 12:14:21 -0400

    + Using Google's closure compiler for JS compression

--- mrc_bundles.py
+++ mrc_bundles.py
@@ -1,15 +1,23 @@
 from flask_assets import Bundle
+from webassets.filter import get_filter
+from webassets.filter.closure import ClosureJS
+from sys import stderr
 import uuid
+import os
 
 
 def gen_uuid() -> str:
     return str(uuid.uuid4().hex)
 
 
+closure: ClosureJS = get_filter('closure_js')
+os.environ['CLOSURE_COMPRESSOR_OPTIMIZATION'] = 'SIMPLE_OPTIMIZATIONS'
+print(type(closure), file=stderr)
+
 bundles = {
 
     'base_js': Bundle('js/util/*.js',
-                        filters='jsmin',
+                        filters=closure,
                         output=f'dist/{gen_uuid()}.js'),
 
     'base_css': Bundle('css/*.css',
@@ -17,10 +25,11 @@ bundles = {
                          output=f'dist/{gen_uuid()}.css'),
 
     'common_js': Bundle('common_bp/js/*.js',
-                      filters='jsmin',
+                      filters=closure,
                       output=f'dist/{gen_uuid()}.js'),
 
     'common_css': Bundle('common_bp/css/*.css',
                         filters='cssmin',
                         output=f'dist/{gen_uuid()}.css')
 }
+
