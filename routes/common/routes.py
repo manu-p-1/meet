fc79551d2384327259e310c74e787064153f64f7
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-10 00:15:08 -0400

    + Added simple routes for login

---
+++ routes/common/routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, render_template, request, jsonify, redirect, flash, session, url_for
+
+common_bp = Blueprint('common_bp', __name__,
+                      template_folder='templates', static_folder='static')
+
+
+@common_bp.route('/login/', methods=['GET'])
+def login(ctx=None):
+    return render_template('login.html')
+
+@common_bp.route('/')
+def landing_page():
+    return redirect(url_for('common_bp.login'))
