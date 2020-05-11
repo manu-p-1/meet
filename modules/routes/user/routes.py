64f29ffd12ad3eccd185d390f6155272a288bf21
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-10 23:46:13 -0400

    + Added: Overview routes

--- modules/routes/user/routes.py
+++ modules/routes/user/routes.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, flash, session, url_for
-# from .forms import LoginForm
 
 user_bp = Blueprint('user_bp', __name__,
                       template_folder='templates', static_folder='static')
+
+
+@user_bp.route('/overview/', methods=['GET'])
+def login(ctx=None):
+    return render_template('overview/dash_overview_partial.html')
