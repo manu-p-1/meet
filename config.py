31444fa5004196ff3cc10b248523285cd97ff1b5
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-10 23:43:04 -0400

    + Fixed: Connection String

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 from urllib.parse import quote_plus
 
+from sqlalchemy import create_engine
+
 '''
 Configuration settings to be used on localhost.
 '''
@@ -17,13 +19,12 @@ class DevelopmentConfig:
     MYSQL_DATABASE_DB = os.environ.get('DB')
     MYSQL_DATABASE_HOST = os.environ.get('DB_HOST')
 
-
     if os.environ.get("ALCHEMY_URI") is None:
-        os.environ['ALCHEMY_URI'] = f"mysql://{MYSQL_DATABASE_USER}:%s@host/database" % quote_plus(MYSQL_DATABASE_PASSWORD)
+        os.environ['ALCHEMY_URI'] = f"mysql://{MYSQL_DATABASE_USER}:%s@localhost/mrcdb" % quote_plus(MYSQL_DATABASE_PASSWORD)
+        print('Exported ALCHEMY_URI\n')
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('ALCHEMY_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 
 '''
 Configuration settings to be used on PythonAnywhere.
