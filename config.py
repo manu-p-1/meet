bffb01633bc9c5d8e335e4938be981fb3767bb6c
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-11 01:48:04 -0400

    + Updated: Added a function that gets called on config

--- config.py
+++ config.py
@@ -1,7 +1,19 @@
 import os
+import sys
+from packaging import version
 from urllib.parse import quote_plus
 
-from sqlalchemy import create_engine
+
+def load_env(dbu, dbpw):
+
+    ver = ".".join(map(str, sys.version_info[:3]))
+    if version.parse(ver) < version.parse("3.7.5"):
+        print(f"PYTHON VERSION IS {ver} BUT REQUIRES 3.7.5 OR HIGHER")
+        exit(1)
+
+    if os.environ.get("ALCHEMY_URI") is None:
+        os.environ['ALCHEMY_URI'] = f"mysql://{dbu}:%s@localhost/mrcdb" % quote_plus(dbpw)
+        print('Exported ALCHEMY_URI\n')
 
 '''
 Configuration settings to be used on localhost.
@@ -9,9 +21,7 @@ Configuration settings to be used on localhost.
 class DevelopmentConfig:
     # SERVER RELOAD ON CODE CHANGE
     DEBUG = True
-
     TEMPLATES_AUTO_RELOAD = True
-
 
     #MySQL setup
     MYSQL_DATABASE_USER = os.environ['DB_USER']
@@ -19,9 +29,7 @@ class DevelopmentConfig:
     MYSQL_DATABASE_DB = os.environ.get('DB')
     MYSQL_DATABASE_HOST = os.environ.get('DB_HOST')
 
-    if os.environ.get("ALCHEMY_URI") is None:
-        os.environ['ALCHEMY_URI'] = f"mysql://{MYSQL_DATABASE_USER}:%s@localhost/mrcdb" % quote_plus(MYSQL_DATABASE_PASSWORD)
-        print('Exported ALCHEMY_URI\n')
+    load_env(dbu=MYSQL_DATABASE_USER, dbpw=MYSQL_DATABASE_PASSWORD)
 
     SQLALCHEMY_DATABASE_URI = os.environ.get('ALCHEMY_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -38,3 +46,4 @@ class ProductionConfig:
     MYSQL_DATABASE_PASSWORD = os.environ['DB_PASS']
     MYSQL_DATABASE_DB = os.environ.get('DB')
     MYSQL_DATABASE_HOST = os.environ.get('DB_HOST')
+
