c187ef5fa615fd872c90bc141d08bbfb29fca639
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-13 15:07:08 -0400

    + Updated: Formatting

--- modules/routes/common/forms.py
+++ modules/routes/common/forms.py
@@ -1,8 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField
+from wtforms import StringField, PasswordField
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import InputRequired,Length,AnyOf
+from wtforms.validators import InputRequired, Length, AnyOf
+
 
 class LoginForm(FlaskForm):
-    email = EmailField("Enter Email",validators=[InputRequired()])
-    password = PasswordField('Enter Password',validators=[InputRequired()])
+    email = EmailField("Enter Email", validators=[InputRequired()])
+    password = PasswordField('Enter Password', validators=[InputRequired()])
