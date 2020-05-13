557a967e093b383eaf519bbd3477f96c65837b20
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-12 23:29:23 -0400

    + Updated: create_plan.js scripts for additional functionality

---
+++ modules/routes/user/static/js/create_plan/create_plan.js
@@ -0,0 +1,87 @@
+$(function () {
+    //For the Date Picker
+    let pickerFormat = 'MM/DD/YYYY hh:mm A';
+    let ref = $("#activeRange");
+
+    ref.daterangepicker({
+        autoUpdateInput: false,
+        timePicker: true,
+        startDate: moment().startOf('minute'),
+        minDate: moment().startOf('minute'),
+        endDate: moment().startOf('hour').add(32, 'hour'),
+        maxDate: moment().startOf('year').add(5, 'year'),
+        locale: {
+            format: pickerFormat
+        }
+    });
+    ref.on('apply.daterangepicker', function (ev, picker) {
+        $(this).val(picker.startDate.format(pickerFormat) + ' - ' + picker.endDate.format(pickerFormat));
+    });
+
+    ref.on('cancel.daterangepicker', function (ev, picker) {
+        $(this).val('');
+    });
+
+    //For the Individual Employee toggle
+    let indivUserToggle = $("#fundIndivEmployeesToggle");
+    let fundIndivEmployeeDiv = $("#employeeIDBoundary");
+
+    indivUserToggle.on("click", function () {
+        if (fundIndivEmployeeDiv.hasClass("d-none")) {
+            indivUserToggle.attr("checked", "checked");
+            fundIndivEmployeeDiv.removeClass("d-none");
+            fundIndivEmployeeDiv.children("input").attr("required")
+        } else {
+            indivUserToggle.removeAttr("checked", "checked");
+            fundIndivEmployeeDiv.addClass("d-none");
+            fundIndivEmployeeDiv.children("input").removeAttr("required");
+        }
+    });
+
+    //For adding employees
+    let nei = $("#addNewEmployeeInput");
+    var newEmployeeCount = 2;
+
+    nei.on("click", addNewEmployee);
+
+    function addNewEmployee() {
+        if (newEmployeeCount <= 12) {
+            let lastChild = $("#employeeIDBoundaryRow").children(":last-child");
+            let template =
+                `<div class="form-group col-md-3">
+                <label for="employee${newEmployeeCount}" class="text-muted additional-employee-input-label">Employee <span class="employeeCount">${newEmployeeCount}</span></label>
+                <div class="input-container">
+                    <input id="employee${newEmployeeCount}" class="employeeIDInput form-control" placeholder="Enter Employee ID" required>
+                    <span class="removeNewEmployeeInput material-icons">remove_circle</span>
+                </div>
+            </div>`;
+
+            lastChild.after(template);
+            newEmployeeCount++;
+        } else {
+            alert("Only a maximum of 12 recipients can receive funds at one time");
+        }
+    }
+
+    //For Removing employees
+    let rei = $(".removeNewEmployeeInput");
+    $(document).on("click", ".removeNewEmployeeInput", function (e) {
+        if (newEmployeeCount > 2) {
+            removeNewEmployee(e);
+        }
+    });
+
+    function removeNewEmployee(e) {
+        let closestGroup = $(e.target).closest(".form-group");
+        let after = $(closestGroup).nextAll().find("label.additional-employee-input-label > .employeeCount");
+
+        for (let labelNum of after) {
+            let x = $(labelNum);
+            x.html(parseInt(x.html()) - 1);
+        }
+        $(closestGroup).remove();
+        newEmployeeCount--;
+    }
+});
+
+
