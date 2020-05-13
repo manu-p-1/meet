562e9ea148e588bc6c3148395ac04ee07bd3aca6
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-13 15:08:02 -0400

    + WTForms configurations

--- modules/routes/user/routes.py
+++ modules/routes/user/routes.py
@@ -1,13 +1,24 @@
 from flask import Blueprint, render_template, request, jsonify, redirect, flash, session, url_for
+from modules.routes.user.forms import CreatePlanForm
+from datetime import date
 
 user_bp = Blueprint('user_bp', __name__,
-                      template_folder='templates', static_folder='static')
+                    template_folder='templates', static_folder='static')
 
 
 @user_bp.route('/overview/', methods=['GET'])
 def overview(ctx=None):
     return render_template('overview/dash_overview_partial.html')
 
+
 @user_bp.route('/create_plan/', methods=['GET'])
 def create_plan():
-    return render_template('create_plan/create_plan_partial.html')
+    current_date = date.today()
+    current_date_fmt = current_date.strftime("%m/%d/%y")
+
+    form = CreatePlanForm()
+    if request.method == 'GET':
+        return render_template('create_plan/create_plan_partial.html', form=form, current_date=current_date_fmt)
+    else:
+        if form.validate_on_submit():
+            return 'temp'
