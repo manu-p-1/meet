75f118c07831eee06dae3be8981676be4284014f
ssyuen <samuel.s.yuen@gmail.com>
2020-05-14 20:54:42 -0400

    + added models for plan and userplan

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from server import db
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.dialects.mysql import VARCHAR
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
+from sqlalchemy.dialects.mysql import VARCHAR,DATETIME, BOOLEAN
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -33,11 +33,40 @@ class Employee(db.Model):
 
     id = Column(Integer, primary_key=True)
     token = Column(String(200), nullable=False, unique=True)
-    firstname = Column(VARCHAR(45), nullable=False)
-    lastname = Column(VARCHAR(45), nullable=False)
+    first_name = Column(VARCHAR(45), nullable=False)
+    last_name = Column(VARCHAR(45), nullable=False)
     user_dept_FK = Column(ForeignKey('department_lookup.token'), nullable=False, index=True)
 
     department_lookup = relationship('DepartmentLookup')
+
+class Plan(db.Model):
+    __tablename__ = 'plan'
+
+    id = Column(Integer, primary_key=True)
+    plan_name = Column(String(200), nullable=False,unique=True)
+    funding_amount = Column(Float,nullable=False)
+    plan_justification = Column(VARCHAR(300),nullable=False)
+    description = Column(VARCHAR(300),nullable=False)
+    date_range = Column(DATETIME,nullable=False)
+    source_fund = Column(String(50),nullable=False)
+    dest_fund = Column(String(50),nullable=False)
+    fund_individuals = Column(BOOLEAN,nullable=False)
+    control_name = Column(VARCHAR(50))
+    control_window = Column(DATETIME)
+    amount_limit = Column(Float)
+    usage_limit = Column(Integer)
+
+class UserPlan(db.Model):
+    __tablename__ = 'user_plan'
+
+    up_plan_FK = Column(ForeignKey('user.id'),nullable=False,index=True)
+    up_user_FK = Column(ForeignKey('plan.id'),nullable=False,index=True)
+
+    user = relationship('Employee')
+    plan = relationship('Plan')
+
+
+
 
 db.drop_all()
 db.create_all()
