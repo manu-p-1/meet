009f393e485428672332d287d38c11fd153309c6
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-16 08:21:04 -0400

    + Added custom validators specific to Employee collection

--- modules/routes/user/custom_validators.py
+++ modules/routes/user/custom_validators.py
@@ -3,19 +3,21 @@ from sys import stderr
 from wtforms import FieldList, StringField
 from wtforms.validators import Optional, DataRequired, ValidationError
 
+from modules.routes.user.custom_fields import EmployeeInfoTextAreaField
+from modules.routes.user.parsers import MRCDateRangeParser, MRCDateRangeException
+
 
 class RequiredIf(DataRequired):
 
-    def __init__(self, other_field_name):
+    def __init__(self, other_field_name, message=None):
         self.other_field_name = other_field_name
-        super(RequiredIf, self).__init__()
+        super(RequiredIf, self).__init__(message=message)
 
     def __call__(self, form, field):
         other_field = form._fields.get(self.other_field_name)
         if other_field.data is None:
             raise Exception('no field named "%s" in form' % self.other_field_name)
 
-        print("Field", field, "\nType: ", type(field), file=stderr)
         if bool(other_field.data):
             super(RequiredIf, self).__call__(form, field)
         else:
@@ -28,13 +30,20 @@ class Unique(object):
             message = f'Duplicate {object_name} are not allowed.'
         self.message = message
 
-    def __call__(self, form, field):
+    def __call__(self, form, fields):
 
-        print("Field", field, "\nType: ", type(field), file=stderr)
-        if type(field) is FieldList:
-            no = self.has_dup(field)
-            if no:
-                raise ValidationError(self.message)
+        if type(fields) is not FieldList:
+            raise Exception("Unique cannot be used on a non-FieldList type")
+
+        if fields.data is None:
+            raise Exception('no field named "%s" in form' % fields)
+
+        if len(fields) == 0 or len(fields) == 1:
+            return False
+
+        no = self.has_dup(fields)
+        if no:
+            raise ValidationError(self.message)
 
     def has_dup(self, list_):
         seen = set()
@@ -45,28 +54,56 @@ class Unique(object):
         return False
 
 
+class EmployeeUnique(object):
+    def __init__(self, message=None, object_name="employee id's"):
+        if not message:
+            message = f'Duplicate {object_name} are not allowed.'
+        self.message = message
+
+    def __call__(self, form, fields):
+
+        if type(fields) is not FieldList:
+            raise Exception("Unique cannot be used on a non-FieldList type")
+
+        if fields.data is None:
+            raise Exception('no field named "%s" in form' % fields)
+
+        if len(fields) == 0 or len(fields) == 1:
+            return False
+
+        no = self.has_dup(fields)
+        if no:
+            raise ValidationError(self.message)
+
+    def has_dup(self, list_):
+        seen = set()
+        for f in list_:
+
+            if type(f) is not EmployeeInfoTextAreaField:
+                raise Exception("EmployeeUnique cannot be used with a non EmployeeInfoTextAreaField type")
+
+            if f.data['id'] in seen:
+                return True
+            seen.add(f.data['id'])
+        return False
+
+
 class DateRange(object):
     def __init__(self, message=None):
         if not message:
-            message = f'Range is malformed. Follow the format MM/DD/YYYY HH:MM AM/PM - MM/DD/YYYY HH:MM AM/PM'
+            message = f'Date range is malformed. Follow the format MM/DD/YYYY HH:MM PM - MM/DD/YYYY HH:MM AM'
         self.message = message
 
     def __call__(self, form, field):
-        print("Field", field, "\nType: ", type(field), file=stderr)
-        print(field.data, file=stderr)
-        # daterange = str(field.data)
-        #
-        # if type(field) is not StringField:
-        #     raise ValidationError(self.message)
-        #
-        # splitted = daterange.split("-")
-        #
-        # start_range = splitted[0]
-        # end_range = splitted[1]
-        #
-        # print(start_range, file=stderr)
-        # print(end_range, file=stderr)
+        if type(field) is not StringField:
+            raise ValidationError(self.message)
 
+        if field.data is None:
+            raise Exception('no field named "%s" in form' % field)
 
-class DateParser:
-    pass
+        drp = MRCDateRangeParser(field.data)
+
+        try:
+            drp.parse()
+        except MRCDateRangeException as mrcdre:
+            raise ValidationError(self.message)
