58e4f4b0f430f9facc041b5bf2714c9d94df439a
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-16 16:43:13 -0400

    + Formatting

--- modules/routes/user/static/js/shards-dashboards.1.1.0.js
+++ modules/routes/user/static/js/shards-dashboards.1.1.0.js
@@ -1,75 +1,76 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
-	typeof define === 'function' && define.amd ? define(factory) :
-	(factory());
-}(this, (function () { 'use strict';
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+        typeof define === 'function' && define.amd ? define(factory) :
+            (factory());
+}(this, (function () {
+    'use strict';
 
-if (typeof Chart === 'undefined') {
-  throw new Error('Shards Dashboard requires the Chart.min.js library in order to function properly.');
-}
-
-window.ShardsDashboards = window.ShardsDashboards ? window.ShardsDashboards : {};
-
-$.extend($.easing, {
-  easeOutSine: function easeOutSine(x, t, b, c, d) {
-    return c * Math.sin(t / d * (Math.PI / 2)) + b;
-  }
-});
-
-/**
- * Chart.min.js - Line Chart with Vertical Line
- */
-Chart.defaults.LineWithLine = Chart.defaults.line;
-Chart.controllers.LineWithLine = Chart.controllers.line.extend({
-  draw: function draw(ease) {
-    Chart.controllers.line.prototype.draw.call(this, ease);
-    if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
-      var activePoint = this.chart.tooltip._active[0],
-          ctx = this.chart.ctx,
-          x = activePoint.tooltipPosition().x,
-          topY = this.chart.scales['y-axis-0'].top,
-          bottomY = this.chart.scales['y-axis-0'].bottom;
-
-      // Draw the line
-      ctx.save();
-      ctx.beginPath();
-      ctx.moveTo(x, topY);
-      ctx.lineTo(x, bottomY);
-      ctx.lineWidth = 0.5;
-      ctx.strokeStyle = '#ddd';
-      ctx.stroke();
-      ctx.restore();
+    if (typeof Chart === 'undefined') {
+        throw new Error('Shards Dashboard requires the Chart.min.js library in order to function properly.');
     }
-  }
-});
 
-$(document).ready(function () {
+    window.ShardsDashboards = window.ShardsDashboards ? window.ShardsDashboards : {};
 
-  /**
-   * Dropdown adjustments
-   */
+    $.extend($.easing, {
+        easeOutSine: function easeOutSine(x, t, b, c, d) {
+            return c * Math.sin(t / d * (Math.PI / 2)) + b;
+        }
+    });
 
-  var slideConfig = {
-    duration: 270,
-    easing: 'easeOutSine'
-  };
+    /**
+     * Chart.min.js - Line Chart with Vertical Line
+     */
+    Chart.defaults.LineWithLine = Chart.defaults.line;
+    Chart.controllers.LineWithLine = Chart.controllers.line.extend({
+        draw: function draw(ease) {
+            Chart.controllers.line.prototype.draw.call(this, ease);
+            if (this.chart.tooltip._active && this.chart.tooltip._active.length) {
+                var activePoint = this.chart.tooltip._active[0],
+                    ctx = this.chart.ctx,
+                    x = activePoint.tooltipPosition().x,
+                    topY = this.chart.scales['y-axis-0'].top,
+                    bottomY = this.chart.scales['y-axis-0'].bottom;
 
-  // Add dropdown animations when toggled.
-  $(':not(.main-sidebar--icons-only) .dropdown').on('show.bs.dropdown', function () {
-    $(this).find('.dropdown-menu').first().stop(true, true).slideDown(slideConfig);
-  });
+                // Draw the line
+                ctx.save();
+                ctx.beginPath();
+                ctx.moveTo(x, topY);
+                ctx.lineTo(x, bottomY);
+                ctx.lineWidth = 0.5;
+                ctx.strokeStyle = '#ddd';
+                ctx.stroke();
+                ctx.restore();
+            }
+        }
+    });
 
-  $(':not(.main-sidebar--icons-only) .dropdown').on('hide.bs.dropdown', function () {
-    $(this).find('.dropdown-menu').first().stop(true, true).slideUp(slideConfig);
-  });
+    $(document).ready(function () {
 
-  /**
-   * Sidebar toggles
-   */
-  $('.toggle-sidebar').click(function (e) {
-    $('.main-sidebar').toggleClass('open');
-  });
-});
+        /**
+         * Dropdown adjustments
+         */
+
+        var slideConfig = {
+            duration: 270,
+            easing: 'easeOutSine'
+        };
+
+        // Add dropdown animations when toggled.
+        $(':not(.main-sidebar--icons-only) .dropdown').on('show.bs.dropdown', function () {
+            $(this).find('.dropdown-menu').first().stop(true, true).slideDown(slideConfig);
+        });
+
+        $(':not(.main-sidebar--icons-only) .dropdown').on('hide.bs.dropdown', function () {
+            $(this).find('.dropdown-menu').first().stop(true, true).slideUp(slideConfig);
+        });
+
+        /**
+         * Sidebar toggles
+         */
+        $('.toggle-sidebar').click(function (e) {
+            $('.main-sidebar').toggleClass('open');
+        });
+    });
 
 })));
 
