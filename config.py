b374d4ba191480d96dc83505b4ac12814dbb43da
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-17 14:57:59 -0400

    + Removed SqlAlchemy related stuff

--- config.py
+++ config.py
@@ -1,14 +1,4 @@
 import os
-import sys
-
-from urllib.parse import quote_plus
-
-
-def load_env(dbu, dbpw):
-    if os.environ.get("ALCHEMY_URI") is None:
-        os.environ['ALCHEMY_URI'] = f"mysql://{dbu}:%s@localhost/mrcdb" % quote_plus(dbpw)
-        print('Exported ALCHEMY_URI\n', file=sys.stderr)
-
 
 '''
 Configuration settings to be used on localhost.
@@ -25,15 +15,6 @@ class DevelopmentConfig:
     MYSQL_DATABASE_PASSWORD = os.environ['DB_PASS']
     MYSQL_DATABASE_DB = os.environ.get('DB')
     MYSQL_DATABASE_HOST = os.environ.get('DB_HOST')
-
-    load_env(dbu=MYSQL_DATABASE_USER, dbpw=MYSQL_DATABASE_PASSWORD)
-
-    SQLALCHEMY_DATABASE_URI = os.environ.get('ALCHEMY_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        "pool_size": 300
-    }
-
 
 '''
 Configuration settings to be used on PythonAnywhere.
