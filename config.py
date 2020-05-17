35c382c213e5db6abfa1f72be16d7b174a7de51d
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-16 21:12:14 -0400

    - Removed: Python version checking

--- config.py
+++ config.py
@@ -1,14 +1,10 @@
 import os
 import sys
-from packaging import version
+
 from urllib.parse import quote_plus
 
-def load_env(dbu, dbpw):
-    ver = ".".join(map(str, sys.version_info[:3]))
-    if version.parse(ver) < version.parse("3.7"):
-        print(f"PYTHON VERSION IS {ver} BUT REQUIRES 3.7 OR HIGHER", file=sys.stderr)
-        exit(1)
 
+def load_env(dbu, dbpw):
     if os.environ.get("ALCHEMY_URI") is None:
         os.environ['ALCHEMY_URI'] = f"mysql://{dbu}:%s@localhost/mrcdb" % quote_plus(dbpw)
         print('Exported ALCHEMY_URI\n', file=sys.stderr)
