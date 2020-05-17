12f73b5c6ff3372325f811cc9dce8be9815f4d18
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-17 19:11:56 -0400

    + Execute load() only if db_init does not exist

--- modules/routes/common/routes.py
+++ modules/routes/common/routes.py
@@ -5,7 +5,8 @@ from .forms import LoginForm
 from server import client
 
 from modules.decorators.utils import check_login
-from ..utils.functions.function_utils import gather_form_errors
+from modules.routes import load_values
+from modules.routes.utils.functions.function_utils import gather_form_errors
 
 common_bp = Blueprint('common_bp', __name__,
                       template_folder='templates', static_folder='static')
@@ -13,10 +14,12 @@ common_bp = Blueprint('common_bp', __name__,
 
 @common_bp.route('/login/', methods=['GET', 'POST'])
 def login(ctx=None):
-    try:
-        load_values()
-    except Exception as e:
-        print(e)    
+    if not session.get('db_init'):
+        try:
+            load_values()
+        except Exception as e:
+            print(e)
+
     if check_login(session):
         return redirect(url_for('user_bp.overview'))
 
@@ -62,34 +65,3 @@ def login(ctx=None):
 @common_bp.route('/')
 def landing_page():
     return redirect(url_for('common_bp.login'))
-
-
-def load_values():
-    conn = mysql.connect()
-    cursor = conn.cursor()
-    print('\n\nINITIALIZING DB\n\n')
-    for i, dept in enumerate(client.departments):
-        query = """
-            INSERT INTO department_lookup (token, department)
-            VALUES (%s,%s)"""
-        cursor.execute(query, (dept.token, client.DEPARTMENT_LIST[i]))
-        print(dept.token + ' has been inserted.')
-
-    for e in client.employees:
-        query = """
-            INSERT INTO employee (token,first_name,last_name,employee_dept_FK) 
-            VALUES (%s ,%s, %s, %s)"""
-        cursor.execute(query, (e.token, e.first_name,
-                               e.last_name, e.parent_token))
-        print(e.token + 'h has been inserted.')
-
-    for dept in client.DEPARTMENT_LIST:
-        query = """
-            INSERT INTO manager (email,pass,first_name,last_name,title,description,manager_dept_FK)
-            VALUES (%s, %s, %s, %s, %s, %s, %s)
-            """
-        cursor.execute(query, (
-            client.MANAGERS[dept]['email'], client.MANAGERS[dept]['pass'], client.MANAGERS[dept]['first_name'],
-            client.MANAGERS[dept]['last_name'], 'Sr. Division Manager', '', client.MANAGERS[dept]['manager_dept_FK']))
-    session['db_init'] = True
-    conn.commit()
