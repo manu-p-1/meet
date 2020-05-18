3c376ba96eb9d69c727671227a4b30bbff2c86f5
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-18 02:00:49 -0400

    + Removed SqlAlchemy based code

--- modules/routes/utils/routes.py
+++ modules/routes/utils/routes.py
@@ -1,3 +1,5 @@
+import sys
+
 from flask import Blueprint, render_template, request, jsonify, redirect, flash, session, url_for
 import json
 from server import mysql
@@ -33,25 +35,34 @@ def overview(ctx=None):
     conn = mysql.connect()
     cursor = conn.cursor()
 
-    dept = request.args.get('department')[:2]
+    dept = request.args.get('department')
     name = request.args.get('term')
+
+    if dept is None or name is None:
+        return jsonify(query_error=True)
+
     query = '''SELECT token FROM department_lookup WHERE department = %s'''
-    cursor.execute(query,(dept))
+    cursor.execute(query, dept)
     dept_token = cursor.fetchall()[0][0]
 
     query = '''SELECT id, first_name, last_name FROM employee WHERE employee_dept_FK = %s'''
-    cursor.execute(query,(dept_token))
+    cursor.execute(query, dept_token)
     employee = cursor.fetchall()
+
+    print(employee, file=sys.stderr)
 
     if name and ' ' in name:
         fname = name[:name.find(' ')].lower()
         lname = name[name.find(' '):].lower()
-        e_payload = [{"name": e.first_name + ' ' + e.last_name, "id": e.id}
-                     for e in employee if fname in e.first_name.lower() and lname in e.last_name.lower()]
+        e_payload = [
+            {"name": f'{e[1]} {e[2]}', "id": e[0]} for e in employee if fname in e[1] and lname in e[2]
+        ]
     else:
         name = name.lower()
-        e_payload = [{"name": e.first_name + ' ' + e.last_name, "id": e.id}
-                     for e in employee if name in e.first_name.lower() or name in e.last_name.lower()]
+        e_payload = [
+            {"name": f'{e[1]} {e[2]}', "id": e[0]}
+            for e in employee if name in e[1].lower() or name in e[2].lower()
+        ]
 
     print(json.dumps(e_payload))
 
