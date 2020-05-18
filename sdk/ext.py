85a4e57a3b9794cd1136da51ab7b97e7849d27d7
ssyuen <samuel.s.yuen@gmail.com>
2020-05-18 18:07:12 -0400

    + conditionall add PeerTransfer, typo on init for simulation

--- sdk/ext.py
+++ sdk/ext.py
@@ -1,10 +1,18 @@
+import json
+
+
 class PeerTransfer:
-    def __init__(self,response: dict):
-        print(response)
-    
+    def __init__(self, response: dict):
         self.token = response['token']
         self.amount = response['amount']
         self.currency_code = response['currency_code']
-        self.sender_business_token = response['sender_business_token']
-        self.recipient_business_token = response['recipient_business_token']
         self.created_time = response['created_time']
+        self.response = response
+        if 'sender_user_token' not in response:
+            self.sender_business_token = response['sender_business_token']
+            self.recipient_business_token = response['recipient_business_token']
+        else:
+            self.sender_user_token = response['sender_business_token']
+            self.recipient_user_token = response['recipient_user_token']
+    def __str__(self):
+        return json.dumps(self.response, indent=4)
