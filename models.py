2c478463ce26fd43d3d38d8b5e73b728e905e285
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-18 13:27:15 -0400

    + Fixed Plan insert

--- models.py
+++ models.py
@@ -1,40 +1,49 @@
+from sys import stderr
+
+
 class Plan:
 
     def __init__(self, cursor, conn=None):
         self._cursor = cursor
         self._conn = conn
         self._generic_insert = '''INSERT INTO plan (plan_name,funding_amount,plan_justification,memo,start_date,end_date,
-                    source_fund_FK,dest_fund_FK,fund_individuals,control_name, control_window,amount_limit,usage_limit,complete) VALUES 
-                    (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)'''
+                    source_fund_FK, dest_fund_FK,fund_individuals,control_name, control_window,amount_limit,usage_limit,complete) VALUES 
+                    (%s,%s,%s,%s,%s,%s,
+                    (SELECT id FROM department_lookup WHERE department = %s),
+                    (SELECT id FROM department_lookup WHERE department = %s),
+                    %s,%s,%s,%s,%s,%s)'''
 
         self._generic_select = """SELECT * FROM plan"""
 
     def insert_with_form(self, form):
-        if form is not None:
-            self._cursor.execute(self._generic_insert, (form.planName.data, form.fundingAmount.data,
-                                                        form.planJustification.data, form.memo.data,
-                                                        form.startDate.data,
-                                                        form.endDate.data, form.sourceFund.data, form.destFund.data,
-                                                        form.fundIndivEmployeesToggle.data,
-                                                        form.controlName.data, form.controlWindow.data,
-                                                        form.amountLimit.data,
-                                                        form.usageLimit.data, False))
-            if self._conn is not None:
-                self._conn.commit()
 
-    def insert_without_form(self, plane_name, funding_amount, plan_justification, description, start_date, end_date,
-                            source_fund, dest_fund, fund_individuals, control_name, control_window, amount_limit,
-                            usage_limit, complete):
-        self._cursor.execute(self._generic_insert, (plane_name, funding_amount,
-                                                    plan_justification.data, description, start_date,
-                                                    end_date, source_fund, dest_fund,
-                                                    fund_individuals,
-                                                    control_name, control_window,
-                                                    amount_limit,
-                                                    usage_limit, complete))
+        for field in form:
+            if field.data == '':
+                field.data = None
+
+        self._cursor.execute(self._generic_insert, (form.planName.data, form.fundingAmount.data,
+                                                    form.planJustification.data, form.memo.data,
+                                                    form.startDate.data,
+                                                    form.endDate.data, form.sourceFund.data, form.destFund.data,
+                                                    form.fundIndivEmployeesToggle.data,
+                                                    form.controlName.data, form.controlWindow.data,
+                                                    form.amountLimit.data,
+                                                    form.usageLimit.data, False))
+
+        v = """SELECT id FROM `plan` WHERE plan_name = (%s)"""
+        self._cursor.execute(v, form.planName.data)
+        plid = self._cursor.fetchone()[0]
+
+        print("EMPLOYEES OPTIONAL", form.employeesOptional.data, file=stderr)
+        if len(form.employeesOptional.data) != 0 and form.employeesOptional.data[0] != '':
+            q = """INSERT INTO `employee_plan`(ep_employee_FK, ep_plan_fk) 
+                                  VALUES ( %s,  %s)"""
+            for employeeField in form.employeesOptional.data:
+                print("EMPLOYEE FIELD", employeeField, file=stderr)
+                self._cursor.execute(q, (employeeField.data['id'], plid))
+
         if self._conn is not None:
             self._conn.commit()
-            self._conn.close()
 
     def select_all(self):
         self._cursor.execute(self._generic_select)
