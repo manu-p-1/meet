754f67341880ea654d1b5035832c6c2e96ff6a8a
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-18 20:35:48 -0400

    + Added 2 logic functions

---
+++ modules/simulation/logic.py
@@ -0,0 +1,32 @@
+import json
+from sys import stderr
+
+from server import client, mysql
+
+
+def department_alloc():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    q = """
+        SELECT amount FROM transaction WHERE src_token = %s
+    """
+    cursor.execute(q, client.business.token)
+    return cursor.fetchall()
+
+
+def department_utilization():
+    conn = mysql.connect()
+    cursor = conn.cursor()
+
+    spending = {}
+    q = """
+        SELECT SUM(amount) AS total_spending FROM transaction WHERE src_token = %s
+        GROUP BY src_token
+    """
+    for dept in client.departments:
+        name = dept.business_name_dba
+        cursor.execute(q, dept.token)
+        spending[name] = cursor.fetchall()
+
+    return spending
