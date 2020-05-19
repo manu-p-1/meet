cc2525e268ed31322099160c512706bfe58b0213
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-19 18:20:43 -0400

    + Abstract Model class

--- models.py
+++ models.py
@@ -1,28 +1,54 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 from sys import stderr
 from datetime import datetime
 
 
 class Model(ABC):
-    pass
 
-
-class Plan:
-
-    def __init__(self, cursor, conn=None):
+    def __init__(self, cursor, conn, generic_insert, generic_select, generic_select_where):
         self._cursor = cursor
         self._conn = conn
-        self._generic_insert = '''INSERT INTO plan (plan_name,funding_amount,plan_justification,memo,start_date,end_date,
+        self._generic_insert = generic_insert
+        self._generic_select = generic_select
+        self._generic_select_where = generic_select_where
+
+    @abstractmethod
+    def insert(self, *args):
+        pass
+
+    def select_all(self):
+        self._cursor.execute(self._generic_select)
+        return self._cursor.fetchall()
+
+    def select_where(self, condition1, condition2):
+        self._cursor.execute(self._generic_select_where, (condition1, condition2))
+        return self._cursor.fetchall()
+
+    @property
+    def get_conn(self):
+        return self._conn
+
+    @property
+    def get_cursor(self):
+        return self._cursor
+
+
+class Plan(Model):
+
+    def __init__(self, cursor, conn):
+
+        insert = '''INSERT INTO plan (plan_name,funding_amount,plan_justification,memo,start_date,end_date,
                     source_fund_FK, dest_fund_FK,fund_individuals,control_name, control_window,amount_limit,usage_limit,complete) VALUES 
                     (%s,%s,%s,%s,%s,%s,
                     (SELECT id FROM department_lookup WHERE department = %s),
                     (SELECT id FROM department_lookup WHERE department = %s),
                     %s,%s,%s,%s,%s,%s)'''
+        select = """SELECT * FROM plan"""
+        select_where = "SELECT * FROM plan WHERE %s = %s"
 
-        self._generic_select = """SELECT * FROM plan"""
-        self._generic_select_where = "SELECT * FROM plan WHERE %s = %s"
+        super().__init__(cursor, conn, insert, select, select_where)
 
-    def insert_with_form(self, form):
+    def insert(self, form):
 
         for field in form:
             if field.data == '':
@@ -50,85 +76,52 @@ class Plan:
                 print("EMPLOYEE FIELD", employeeField, file=stderr)
                 self._cursor.execute(q, (employeeField['id'], plid))
 
-        if self._conn is not None:
-            self._conn.commit()
-
-    def select_all(self):
-        self._cursor.execute(self._generic_select)
-        return self._cursor.fetchall()
-
-    def select_where(self, condition1, condition2):
-        self._cursor.execute(self._generic_select_where, (condition1, condition2))
-        return self._cursor.fetchall()
+        self._conn.commit()
 
 
-class Manager:
+class Manager(Model):
 
-    def __init__(self, cursor, conn=None):
-        self._cursor = cursor
-        self._conn = conn
-        self._generic_insert = '''
-        INSERT INTO manager(email, pass, first_name, last_name, title, description, manager_dept_FK) VALUES 
+    def __init__(self, cursor, conn):
+        insert = '''INSERT INTO manager(email, pass, first_name, last_name, title, description, manager_dept_FK) VALUES 
                     (%s,%s,%s,%s,%s,%s,%s)'''
+        select = """SELECT * FROM manager"""
+        select_where = """SELECT * FROM manager where %s = %s"""
 
-        self._generic_select = """SELECT * FROM manager"""
+        super().__init__(cursor, conn, insert, select, select_where)
 
-    def insert(self, email, pass_, first_name, last_name, title, description, manager_dept_FK):
+    def insert(self, email, pass_, first_name, last_name, title, description, manager_dept_FK, loop=False):
         self._cursor.execute(self._generic_insert,
                              (email, pass_, first_name, last_name, title, description, manager_dept_FK))
-
-        if self._conn is not None:
-            self._conn.commit()
-            self._conn.close()
-
-    def select_all(self):
-        self._cursor.execute(self._generic_select)
-        return self._cursor.fetchall()
+        self._conn.commit()
 
 
-class Employee:
+class Employee(Model):
 
-    def __init__(self, cursor, conn=None):
-        self._cursor = cursor
-        self._conn = conn
-        self._generic_insert = '''
-        INSERT INTO employee(token, first_name, last_name, employee_dept_FK) VALUES 
-                    (%s,%s,%s,%s)'''
+    def __init__(self, cursor, conn):
+        insert = '''INSERT INTO employee(token, first_name, last_name, employee_dept_FK) VALUES (%s,%s,%s,%s)'''
+        select = """SELECT * FROM employee"""
+        select_where = """SELECT * FROM employee where %s = %s"""
 
-        self._generic_select = """SELECT * FROM employee"""
+        super().__init__(cursor, conn, insert, select, select_where)
 
     def insert(self, token, first_name, last_name, employee_dept_FK):
         self._cursor.execute(self._generic_insert, (token, first_name, last_name, employee_dept_FK))
-
-        if self._conn is not None:
-            self._conn.commit()
-            self._conn.close()
-
-    def select_all(self):
-        self._cursor.execute(self._generic_select)
-        return self._cursor.fetchall()
+        self._conn.commit()
 
 
-class Transaction:
+class Transaction(Model):
 
-    def __init__(self, cursor, conn=None):
-        self._cursor = cursor
-        self._conn = conn
-        self._generic_insert = '''
-        INSERT INTO transaction(src_token, dest_token, create_time, amount) VALUES (%s, %s, %s, %s)'''
+    def __init__(self, cursor, conn):
+        insert = '''INSERT INTO transaction(src_token, dest_token, create_time, amount) VALUES (%s, %s, %s, %s)'''
+        select = """SELECT * FROM transaction"""
+        select_where = """SELECT * FROM transaction WHERE %s = %s"""
 
-        self._generic_select = """SELECT * FROM transaction"""
+        super().__init__(cursor, conn, insert, select, select_where)
 
     def insert(self, src_token, dest_token, create_time, amount):
         self._cursor.execute(self._generic_insert, (src_token, dest_token, create_time, amount))
+        self._conn.commit()
 
-        if self._conn is not None:
-            self._conn.commit()
-            self._conn.close()
-
-    def select_all(self):
-        self._cursor.execute(self._generic_select)
-        return self._cursor.fetchall()
-    
-    def current_time(self, transaction_time):
+    @classmethod
+    def current_time(cls, transaction_time):
         return datetime.strptime(transaction_time, "%Y-%m-%dT%H:%M:%SZ")
