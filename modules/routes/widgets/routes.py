89d43577c3a11cfbc7a886a3a95995750767c405
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-20 12:35:47 -0400

    + Fixed runtime fetch error & added login_req

--- modules/routes/widgets/routes.py
+++ modules/routes/widgets/routes.py
@@ -1,3 +1,4 @@
+from modules.decorators.utils import login_required
 from server import mysql
 from flask import Blueprint, render_template, request, jsonify, redirect, flash, session, url_for
 from server import client
@@ -8,35 +9,42 @@ widgets_bp = Blueprint('widgets_bp', __name__)
 
 
 @widgets_bp.route('/dash/department_allocation/', methods=['GET'])
+@login_required(session)
 def wid_department_allocation():
     return jsonify(department_alloc())
 
 
 @widgets_bp.route('/dash/department_utilization/', methods=['GET'])
+@login_required(session)
 def wid_department_utilization():
     return jsonify(department_utilization())
 
 
 @widgets_bp.route('/dash/current_business_balance/', methods=['GET'])
+@login_required(session)
 def wid_current_business_balance():
     return jsonify(current_business_balance())
 
 
 @widgets_bp.route('/dash/department_balance/', methods=['GET'])
+@login_required(session)
 def wid_department_balance():
     return jsonify(department_balance(session['manager_dept']))
 
 
 @widgets_bp.route('/dash/department_employee_count/', methods=['GET'])
+@login_required(session)
 def wid_department_employee_count():
     return jsonify(amount=department_employee_count(session['manager_dept']))
 
 
 @widgets_bp.route('/dash/current_outgoing_transactions/', methods=['GET'])  # Doesn't work
+@login_required(session)
 def wid_current_outgoing_transactions():
-    return jsonify(total=current_outgoing_transactions(session['manager_dept']))
+    return jsonify(current_outgoing_transactions(session['manager_dept']))
 
 
 @widgets_bp.route('/dash/active_plans/', methods=['GET'])
+@login_required(session)
 def wid_active_plans():
     return jsonify(total=active_plans())
