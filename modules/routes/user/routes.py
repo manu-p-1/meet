42e5779129108854f703c37fada50309233fab67
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-21 17:41:27 -0400

    + Moved alert partial so it can be used by manage form as well

--- modules/routes/user/routes.py
+++ modules/routes/user/routes.py
@@ -80,14 +80,14 @@ def create_plan():
             return jsonify(
                 status=True,
                 response=render_template(
-                    'create_plan/alert_partial.html', status=True)
+                    'alert_partial.html', status=True)
             )
         else:
             print(form.errors.items(), file=stderr)
             return jsonify(
                 status=False,
                 response=render_template(
-                    'create_plan/alert_partial.html', form=form, status=False)
+                    'alert_partial.html', form=form, status=False)
             )
 
 
@@ -121,5 +121,5 @@ def manage_plan():
             return jsonify(
                 status=True,
                 response=render_template(
-                    'create_plan/alert_partial.html', status=True)
+                    'alert_partial.html', status=True)
             )
