f64cf88f5e59948df83e4577f2b9063bea28b590
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-22 11:45:52 -0400

    + Manipulated return data by adding currency

--- modules/routes/widgets/routes.py
+++ modules/routes/widgets/routes.py
@@ -1,8 +1,12 @@
+import locale
+from sys import stderr
+
 from modules.decorators.utils import login_required
 from flask import Blueprint, render_template, request, jsonify, redirect, flash, session, url_for
 from modules.simulation.logic import department_alloc, department_utilization, current_business_balance, \
     department_balance, department_employee_count, current_outgoing_transactions, active_plans, \
-    department_employee__monthly_spending, plan_overview_six_months
+    department_employee__monthly_spending, plan_avg_six_months
+from server import client
 
 widgets_bp = Blueprint('widgets_bp', __name__)
 
@@ -22,13 +26,21 @@ def wid_department_utilization():
 @widgets_bp.route('/dash/current_business_balance/', methods=['GET'])
 @login_required(session)
 def wid_current_business_balance():
-    return jsonify(current_business_balance())
+    sendable = current_business_balance()
+    locale.setlocale(locale.LC_ALL, '')
+    for key, val in sendable.items():
+        sendable[key] = locale.currency(round(val), grouping=True)
+    return jsonify(sendable)
 
 
 @widgets_bp.route('/dash/department_balance/', methods=['GET'])
 @login_required(session)
 def wid_department_balance():
-    return jsonify(department_balance(session['manager_dept']))
+    sendable = department_balance(session['manager_dept'])
+    locale.setlocale(locale.LC_ALL, '')
+    for key, val in sendable.items():
+        sendable[key] = locale.currency(round(val), grouping=True)
+    return jsonify(sendable)
 
 
 @widgets_bp.route('/dash/department_employee_count/', methods=['GET'])
@@ -52,10 +64,18 @@ def wid_active_plans():
 @widgets_bp.route('/dash/department_employee__monthly_spending/', methods=['GET'])
 @login_required(session)
 def wid_department_employee__monthly_spending():
-    return jsonify(department_employee__monthly_spending(session['manager_dept']))
+    sendable = department_employee__monthly_spending(session['manager_dept'])
+    locale.setlocale(locale.LC_ALL, '')
+    for key, value in sendable.items():
+        value['gpa_bal'] = locale.currency(value['gpa_bal'])
+        value['monthly_spending'] = locale.currency(value['monthly_spending'], grouping=True)
+    return jsonify(sendable)
 
 
-@widgets_bp.route('/dash/plan_overview_six_months/', methods=['GET'])
+@widgets_bp.route('/dash/plan_avg_six_months/', methods=['GET'])
 @login_required(session)
-def wid_plan_overview_six_months():
-    return jsonify(plan_overview_six_months(session['manager_dept']))
+def wid_plan_avg_six_months():
+    return jsonify(
+        data=plan_avg_six_months(session['manager_dept']),
+        department=client.READABLE_DEPARTMENTS[session['manager_dept']]
+    )
