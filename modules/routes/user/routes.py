ca3d2e3c37e700eed2a696024836b6ffd75ab2dc
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-24 02:55:31 -0400

    + Refactored code, added manage_plan logic and optimized imports

--- modules/routes/user/routes.py
+++ modules/routes/user/routes.py
@@ -1,12 +1,13 @@
 from models import Plan
+from modules.routes.utils.classes.class_utils import ManipulationType
+from modules.routes.utils.functions.function_utils import is_duplicate_plan, is_active_plan, short_error
 from server import mysql, client
 from datetime import date
 from sys import stderr
 from flask import Blueprint, render_template, request, jsonify, redirect, flash, session, url_for
-from modules.routes.user.forms import create_plan_form
+from modules.routes.user.forms import create_plan_form, get_plan_form
 from modules.decorators.utils import login_required
 from modules.middleware.logic import executerOrders
-from modules.routes.utils.forms import get_empty_manage_form, get_plan_form
 
 user_bp = Blueprint('user_bp', __name__,
                     template_folder='templates', static_folder='static')
@@ -46,9 +47,6 @@ def logout(ctx=None):
 @user_bp.route('/create_plan/', methods=['GET', 'POST'])
 @login_required(session)
 def create_plan():
-    current_date = date.today()
-    current_date_fmt = current_date.strftime("%m/%d/%Y")
-
     fund_choices = client.DEPT_MAPPINGS
 
     if not session.get('create_plan_visited'):
@@ -58,7 +56,7 @@ def create_plan():
     form = create_plan_form(session, fund_choices)
 
     if request.method == 'GET':
-        return render_template('create_plan/create_plan_partial.html', form=form, current_date=current_date_fmt)
+        return render_template('plans/create_plan/create_plan_partial.html', form=form, current_date=time_now())
     else:
         if form.validate_on_submit():
             print(request.form, file=stderr)
@@ -66,43 +64,43 @@ def create_plan():
             conn = mysql.connect()
             cursor = conn.cursor()
 
+            if is_duplicate_plan(mysql, form.planName.data):
+                return short_error(err_list=['This plan is already exists'])
+
             p = Plan(cursor, conn=conn)
             p.insert(form)
 
             executerOrders()
-            
+
             return jsonify(
-                status=True,
+                response_status="success",
                 response=render_template(
-                    'alert_partial.html', status=True)
+                    'alert_partial.html', status=True, manip_type=ManipulationType.CREATED.value)
             )
         else:
             print(form.errors.items(), file=stderr)
             return jsonify(
-                status=False,
+                response_status="error",
                 response=render_template(
                     'alert_partial.html', form=form, status=False)
             )
 
 
 @user_bp.route('/manage_plan/', methods=['GET', 'POST'])
-# @login_required(session)
+@login_required(session)
 def manage_plan():
+    fund_choices = client.DEPT_MAPPINGS
+    if not session.get('create_plan_visited'):
+        session['create_plan_visited'] = True
+        fund_choices.insert(0, ('', 'Please choose a fund destination'))
+
+    form = get_plan_form(session, fund_choices)
 
     if request.method == 'GET':
-        current_date = date.today()
-        current_date_fmt = current_date.strftime("%m/%d/%Y")
-
-        ef = get_empty_manage_form(session, client.DEPT_MAPPINGS)
-        return render_template('manage_plan/manage_plan_partial.html', current_date=current_date, form=ef)
+        return render_template('plans/manage_plan/manage_plan_partial.html', form=form, current_date=time_now())
     else:
         formatted_plan = session['MANAGE_FORM']
-
-        form = get_plan_form(formatted_plan, session, client.DEPT_MAPPINGS)
-        for field in form:
-            print(f'printing field data ----> {field.data}')
-            print(f'type of field data ----> {type(field.data)}')
-            field = None
+        form = get_plan_form(session, client.DEPT_MAPPINGS)
 
         if form.validate_on_submit():
             print(request.form, file=stderr)
@@ -110,17 +108,33 @@ def manage_plan():
             conn = mysql.connect()
             cursor = conn.cursor()
 
+            if formatted_plan['plan_name'] != form.planName.data:
+                if is_duplicate_plan(mysql, form.planName.data):
+                    return short_error(err_list=['This plan is already exists'])
+
+            if formatted_plan['is_active']:
+                return short_error(err_list=['This plan is currently active'])
+
             p = Plan(cursor, conn=conn)
             p.update(form, formatted_plan['id'])
 
+            """
+            We get plan.employeesOptional.data
+
+            We get a list of employees associated with the old plan through a query.
+            We delete any employee in employee_plan that is not in the list of employees
+
+            """
+
             return jsonify(
-                status=True,
+                response_status="success",
                 response=render_template(
-                    'alert_partial.html', status=True)
+                    'alert_partial.html', status=True, manip_type=ManipulationType.UPDATED.value)
             )
         else:
-            return jsonify(
-                status=False,
-                response=render_template(
-                    'alert_partial.html', status=False)
-            )
+            return short_error(form=form)
+
+
+def time_now() -> str:
+    current_date = date.today()
+    return current_date.strftime("%m/%d/%Y")
