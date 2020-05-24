894da91bd48e71ba8bef4bf96e0108962613e14c
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-24 02:45:01 -0400

    + Moved is_duplicate_plan and is_active_plan funcs to utils dir

--- modules/routes/utils/functions/function_utils.py
+++ modules/routes/utils/functions/function_utils.py
@@ -1,6 +1,57 @@
+from datetime import datetime, timezone
+
+from flask import render_template, jsonify
+
+
 def gather_form_errors(form):
     ll = []
     for field, errors in form.errors.items():
         for error in form.errors[field]:
             ll.append(error)
     return ll
+
+
+def is_duplicate_plan(mysql, field_data) -> bool:
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    q = '''SELECT plan_name FROM plan WHERE plan_name = %s'''
+    cursor.execute(q, field_data)
+    if len(cursor.fetchall()) == 0:
+        return False
+    else:
+        return True
+
+
+def is_active_plan(mysql, field_data) -> bool:
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    now = datetime.now(timezone.utc)
+    start_date = now.strftime("%Y-%m-%d %H:%M:%S")
+    q = '''SELECT plan_name FROM plan WHERE plan_name = %s AND start_date < %s'''
+    cursor.execute(q, (field_data, start_date))
+    if len(cursor.fetchall()) == 0:
+        return False
+    else:
+        return True
+
+
+def short_error(form=None, err_list=None):
+    if form is not None and err_list is None:
+        return jsonify(
+            response_status="error",
+            response=render_template(
+                'alert_partial.html',
+                status=False,
+                form=form
+            )
+        )
+    elif form is None and err_list is not None:
+        return jsonify(
+            response_status="error",
+            response=render_template(
+                'alert_partial.html',
+                status=False,
+                err_list=err_list
+            )
+        )
+    raise Exception('form and err_list are mutually exclusive')
