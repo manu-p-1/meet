de93f5ac97c5bb93f0574015a406f180b21e523f
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-25 02:36:01 -0400

    + Added an operation type enum for adding/deleting employees

--- modules/routes/utils/classes/class_utils.py
+++ modules/routes/utils/classes/class_utils.py
@@ -5,3 +5,8 @@ class ManipulationType(enum.Enum):
     UPDATED = 'updated'
     DELETED = 'deleted'
     CREATED = 'created'
+
+
+class OperationType(enum.Enum):
+    ADD_EMPLOYEE = 'add'
+    DELETE_EMPLOYEE = 'del'
