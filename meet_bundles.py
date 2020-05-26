92d7c0ed3493b8794f363ec5bad2c3dbc51a1f95
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-26 00:27:20 -0400

    + Switched to jsmin...for now

--- meet_bundles.py
+++ meet_bundles.py
@@ -10,14 +10,13 @@ def gen_uuid() -> str:
     return str(uuid.uuid4().hex)
 
 
-closure: ClosureJS = get_filter('closure_js')
-os.environ['CLOSURE_COMPRESSOR_OPTIMIZATION'] = 'SIMPLE_OPTIMIZATIONS'
-print(type(closure), file=stderr)
+# closure: ClosureJS = get_filter('closure_js')
+# os.environ['CLOSURE_COMPRESSOR_OPTIMIZATION'] = 'SIMPLE_OPTIMIZATIONS'
 
 bundles = {
 
     'base_js': Bundle('js/util/*.js',
-                      filters=closure,
+                      filters='jsmin',
                       output=f'dist/{gen_uuid()}.js'),
 
     'base_css': Bundle('css/*.css',
@@ -25,7 +24,7 @@ bundles = {
                        output=f'dist/{gen_uuid()}.css'),
 
     'common_js': Bundle('common_bp/js/*.js',
-                        filters=closure,
+                        filters='jsmin',
                         output=f'dist/{gen_uuid()}.js'),
 
     'common_css': Bundle('common_bp/css/*.css',
@@ -34,7 +33,7 @@ bundles = {
 
     'dash_js': Bundle('user_bp/js/shards-dashboards.1.1.0.js',
                       'user_bp/js/shards.js',
-                      filters=closure,
+                      filters='jsmin',
                       output=f'dist/dash/{gen_uuid()}.js'),
 
     "dash_css": Bundle('user_bp/css/dash-base.css',
@@ -43,7 +42,7 @@ bundles = {
                        output=f'dist/dash/{gen_uuid()}.css'),
 
     "dash_overview_js": Bundle('user_bp/js/overview/*.js',
-                               filters=closure,
+                               filters='jsmin',
                                output=f'dist/dash/overview/{gen_uuid()}.js'),
 
     "dash_overview_css": Bundle('user_bp/css/overview/*.css',
@@ -56,11 +55,11 @@ bundles = {
 
     "dash_create_plan_js": Bundle('user_bp/js/form_common.js',
                                   'user_bp/js/create_plan/*.js',
-                                  filters=closure,
+                                  filters='jsmin',
                                   output=f'dist/dash/create_plan/{gen_uuid()}.js'),
 
     "dash_manage_plan_js": Bundle('user_bp/js/form_common.js',
                                   'user_bp/js/manage_plan/*.js',
-                                  filters=closure,
+                                  filters='jsmin',
                                   output=f'dist/dash/manage_plan/{gen_uuid()}.js')
 }
