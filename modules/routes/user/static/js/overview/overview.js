09df925feb4e4ed3afda2d9e742abb1a9a48ad05
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-25 22:50:09 -0400

    + Added logic for overall spending

--- modules/routes/user/static/js/overview/overview.js
+++ modules/routes/user/static/js/overview/overview.js
@@ -1,9 +1,5 @@
 $(document).ready(function () {
 
-    // Blog overview date range init.
-    $('#blog-overview-date-range').datepicker({});
-
-    //
     // Small Stats
 
     const ERROR = "An error occurred.";
@@ -83,110 +79,125 @@ $(document).ready(function () {
     // Blog Overview Users
     //
 
-    var bouCtx = document.getElementsByClassName('blog-overview-users')[0];
+    var bouCtx = $('#overallEmployeeSpending')[0];
+    getOverallEmployeeSpending();
 
-    // Data
-    var bouData = {
-        // Generate the days labels on the X axis.
-        labels: Array.from(new Array(30), function (_, i) {
-            return i === 0 ? 1 : i;
-        }),
-        datasets: [{
-            label: 'Current Month',
-            fill: 'start',
-            data: [500, 800, 320, 180, 240, 320, 230, 650, 590, 1200, 750, 940, 1420, 1200, 960, 1450, 1820, 2800, 2102, 1920, 3920, 3202, 3140, 2800, 3200, 3200, 3400, 2910, 3100, 4250],
-            backgroundColor: 'rgba(0,123,255,0.1)',
-            borderColor: 'rgba(0,123,255,1)',
-            pointBackgroundColor: '#ffffff',
-            pointHoverBackgroundColor: 'rgb(0,123,255)',
-            borderWidth: 1.5,
-            pointRadius: 0,
-            pointHoverRadius: 3
-        }, {
-            label: 'Past Month',
-            fill: 'start',
-            data: [380, 430, 120, 230, 410, 740, 472, 219, 391, 229, 400, 203, 301, 380, 291, 620, 700, 300, 630, 402, 320, 380, 289, 410, 300, 530, 630, 720, 780, 1200],
-            backgroundColor: 'rgba(255,65,105,0.1)',
-            borderColor: 'rgba(255,65,105,1)',
-            pointBackgroundColor: '#ffffff',
-            pointHoverBackgroundColor: 'rgba(255,65,105,1)',
-            borderDash: [3, 3],
-            borderWidth: 1,
-            pointRadius: 0,
-            pointHoverRadius: 2,
-            pointBorderColor: 'rgba(255,65,105,1)'
-        }]
-    };
+    function getOverallEmployeeSpending() {
+        let loader = $("#overallEmployeeSpendingLoading");
+        let loaderText = $("#overallEmployeeSpendingText");
 
-    // Options
-    var bouOptions = {
-        responsive: true,
-        legend: {
-            position: 'top'
-        },
-        elements: {
-            line: {
-                // A higher value makes the line look skewed at this ratio.
-                tension: 0.3
-            },
-            point: {
-                radius: 0
-            }
-        },
-        scales: {
-            xAxes: [{
-                gridLines: false,
-                ticks: {
-                    callback: function (tick, index) {
-                        // Jump every 7 values on the X axis labels to avoid clutter.
-                        return index % 7 !== 0 ? '' : tick;
-                    }
-                }
-            }],
-            yAxes: [{
-                ticks: {
-                    suggestedMax: 45,
-                    callback: function (tick, index, ticks) {
-                        if (tick === 0) {
-                            return tick;
-                        }
-                        // Format the amounts using Ks for thousands.
-                        return tick > 999 ? (tick / 1000).toFixed(1) + 'K' : tick;
-                    }
-                }
+        $.getJSON('/user/widgets/dash/monthly_employee_spending/', function (dataReturned) {
+            let prevMonthData = dataReturned['data']['previous_month'];
+            let currentMonthData = dataReturned['data']['current_month'];
+
+            const prevMonthKeys = Object.keys(prevMonthData);
+            const overallSpendingDataPast = Object.values(prevMonthData);
+
+            const currentMonthKeys = Object.keys(currentMonthData);
+            const overallSpendingDataCurrent = Object.values(currentMonthData);
+
+            const overallSpendingLabels = [...new Set([...prevMonthKeys, ...currentMonthKeys])];
+            loader.remove();
+
+            renderOverallEmployeeSpendingChart(overallSpendingLabels,
+                overallSpendingDataCurrent,
+                overallSpendingDataPast)
+        }).fail(function () {
+            loaderText.text(ERROR);
+        });
+    }
+
+    function renderOverallEmployeeSpendingChart(labels, curr, past) {
+        console.log("entered");
+        // Data
+        var bouData = {
+            // Generate the days labels on the X axis.
+            labels: labels,
+            datasets: [{
+                label: 'Current Month',
+                fill: 'start',
+                data: curr,
+                backgroundColor: 'rgba(103,58,183,0.1)',
+                borderColor: 'rgb(139,69,230)',
+                pointBackgroundColor: '#ffffff',
+                pointHoverBackgroundColor: 'rgb(139,69,230)',
+                borderWidth: 1.5,
+                pointRadius: 0,
+                pointHoverRadius: 3
+            }, {
+                label: 'Past Month',
+                fill: 'start',
+                data: past,
+                backgroundColor: 'rgba(33,150,243,0.1)',
+                borderColor: 'rgb(33,150,243)',
+                pointBackgroundColor: '#ffffff',
+                pointHoverBackgroundColor: 'rgb(33,150,243)',
+                borderDash: [3, 3],
+                borderWidth: 1,
+                pointRadius: 0,
+                pointHoverRadius: 2,
+                pointBorderColor: 'rgba(255,65,105,1)'
             }]
-        },
-        // Uncomment the next lines in order to disable the animations.
-        // animation: {
-        //   duration: 0
-        // },
-        hover: {
-            mode: 'nearest',
-            intersect: false
-        },
-        tooltips: {
-            custom: false,
-            mode: 'nearest',
-            intersect: false
-        }
-    };
+        };
 
-    // Generate the Analytics Overview chart.
-    window.BlogOverviewUsers = new Chart(bouCtx, {
-        type: 'LineWithLine',
-        data: bouData,
-        options: bouOptions
-    });
+        // Options
+        var bouOptions = {
+            responsive: true,
+            legend: {
+                position: 'top'
+            },
+            elements: {
+                line: {
+                    // A higher value makes the line look skewed at this ratio.
+                    tension: 0.3
+                },
+                point: {
+                    radius: 0
+                }
+            },
+            scales: {
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Day of the Month'
+                    }
+                }],
+                yAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Amount'
+                    }
+                }]
+            },
+            hover: {
+                mode: 'nearest',
+                intersect: false
+            },
+            tooltips: {
+                custom: false,
+                mode: 'nearest',
+                intersect: false
+            }
+        };
 
-    // Hide initially the first and last analytics overview chart points.
-    // They can still be triggered on hover.
-    var aocMeta = BlogOverviewUsers.getDatasetMeta(0);
-    aocMeta.data[0]._model.radius = 0;
-    aocMeta.data[bouData.datasets[0].data.length - 1]._model.radius = 0;
+        // Generate the Analytics Overview chart.
+        window.OverallEmployeeSpending = new Chart(bouCtx, {
+            type: 'LineWithLine',
+            data: bouData,
+            options: bouOptions
+        });
 
-    // Render the chart.
-    window.BlogOverviewUsers.render();
+        // // Hide initially the first and last analytics overview chart points.
+        // // They can still be triggered on hover.
+        var aocMeta = OverallEmployeeSpending.getDatasetMeta(0);
+        aocMeta.data[0]._model.radius = 0;
+        aocMeta.data[bouData.datasets[0].data.length - 1]._model.radius = 0;
 
+        // Render the chart.
+        window.OverallEmployeeSpending.render();
+    }
 
     //
     // Plan avg line graph
@@ -199,7 +210,6 @@ $(document).ready(function () {
         xyDataLabels = Object.keys(data['data']);
         planDepartment = data['department'];
         $.each(data['data'], function (key, val) {
-            console.log(key, val);
             let point = {};
             point["x"] = key;
             point["y"] = val['avg'];
@@ -212,8 +222,6 @@ $(document).ready(function () {
         ref.text(ERROR);
         ref.prev().remove()
     });
-
-    console.info("XYDATA", xyData);
 
     function loadAvgData() {
         var config = {
@@ -309,8 +317,6 @@ $(document).ready(function () {
     };
 
     function setDepartmentOptions(departmentList, amountList, chartCanvasSelection) {
-        console.log(departmentList);
-        console.log(amountList);
 
         let backgroundColor = [];
         let alphaBase = departmentList.length * 0.15;
@@ -373,10 +379,7 @@ $(document).ready(function () {
         ]
     }).container().children().each(function () {
         $(this).addClass("dropdown-item");
-        console.log($(this))
     });
-
-    console.log(buttons);
     $("#exportOptionDropDown").append(buttons);
 
     function loadSpending() {
