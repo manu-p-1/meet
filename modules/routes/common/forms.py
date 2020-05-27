3deb8dd5bf4e75a5b3bf45f9ecfee62e6b520818
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-27 14:24:49 -0400

    + Used custom submit field

--- modules/routes/common/forms.py
+++ modules/routes/common/forms.py
@@ -1,15 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import PasswordField, BooleanField
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import InputRequired, Length, AnyOf
+from wtforms.validators import InputRequired, Length
+
+from modules.routes.common.custom_fields import InlineSubmitField
 
 
 class LoginForm(FlaskForm):
     email = EmailField("Enter Email", validators=[
         InputRequired("An email address is required"),
         Length(min=1, max=255, message="Something went wrong, please try again.")
-    ])
+    ], render_kw={"class": "form-control"})
+
     password = PasswordField('Enter Password', validators=[
         InputRequired("A password is required"),
         Length(min=1, max=255, message="Something went wrong, please try again.")
-    ])
+    ], render_kw={"class": "form-control"})
+
+    loginBtn = InlineSubmitField("Login", default="Login", render_kw={"class": "btn btn-block btn-sm grey lighten-2"})
+
+    rememberMe = BooleanField("Remember Me", render_kw={"class":"custom-control-input"})
