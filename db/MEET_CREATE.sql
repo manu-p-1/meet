80c1a15a1c33750e5c0129d99f2e3fecaec9ff78
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-28 01:31:43 -0400

    + Added column for priority

--- db/MEET_CREATE.sql
+++ db/MEET_CREATE.sql
@@ -57,35 +57,33 @@ CREATE TABLE `meetdb`.`employee`
 -- -----------------------------------------------------
 -- Table `meetdb`.`plan`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `meetdb`.`plan`
+CREATE TABLE `meetdb`.`plan`
 (
-    `id`                 INT            NOT NULL AUTO_INCREMENT,
-    `plan_name`          VARCHAR(200)   NOT NULL,
-    `funding_amount`     DECIMAL(12, 2) NOT NULL,
-    `plan_justification` VARCHAR(50)    NOT NULL,
-    `memo`               VARCHAR(255)   NOT NULL,
-    `start_date`         DATETIME       NOT NULL,
-    `end_date`           DATETIME       NULL     DEFAULT NULL,
-    `source_fund_FK`     INT            NOT NULL,
-    `dest_fund_FK`       INT            NOT NULL,
-    `fund_individuals`   BIT(1)         NOT NULL DEFAULT b'0',
-    `control_name`       VARCHAR(50)    NULL     DEFAULT NULL,
-    `control_window`     VARCHAR(30)    NULL     DEFAULT NULL,
-    `amount_limit`       DECIMAL(12, 2) NULL     DEFAULT NULL,
-    `usage_limit`        INT            NULL     DEFAULT NULL,
-    `complete`           BIT(1)         NOT NULL DEFAULT b'0',
+    `id`                 int            NOT NULL AUTO_INCREMENT,
+    `plan_name`          varchar(200)   NOT NULL,
+    `funding_amount`     decimal(12, 2) NOT NULL,
+    `plan_justification` varchar(50)    NOT NULL,
+    `memo`               varchar(255)   NOT NULL,
+    `start_date`         datetime       NOT NULL,
+    `end_date`           datetime                DEFAULT NULL,
+    `source_fund_FK`     int            NOT NULL,
+    `dest_fund_FK`       int            NOT NULL,
+    `fund_individuals`   bit(1)         NOT NULL DEFAULT b'0',
+    `control_name`       varchar(50)             DEFAULT NULL,
+    `control_window`     varchar(30)             DEFAULT NULL,
+    `amount_limit`       decimal(12, 2)          DEFAULT NULL,
+    `usage_limit`        int                     DEFAULT NULL,
+    `priority`           varchar(45)    NOT NULL,
+    `complete`           bit(1)         NOT NULL DEFAULT b'0',
     PRIMARY KEY (`id`),
-    UNIQUE INDEX `plan_name_UNIQUE` (`plan_name` ASC) VISIBLE,
-    INDEX `dest_fund_FK_idx` (`source_fund_FK` ASC, `dest_fund_FK` ASC) VISIBLE,
-    INDEX `dest_fund_FK_idx1` (`dest_fund_FK` ASC) VISIBLE,
-    CONSTRAINT `dest_fund_FK`
-        FOREIGN KEY (`dest_fund_FK`)
-            REFERENCES `meetdb`.`department_lookup` (`id`),
-    CONSTRAINT `source_fund_FK`
-        FOREIGN KEY (`source_fund_FK`)
-            REFERENCES `meetdb`.`department_lookup` (`id`)
+    UNIQUE KEY `plan_name_UNIQUE` (`plan_name`),
+    KEY `dest_fund_FK_idx` (`source_fund_FK`, `dest_fund_FK`),
+    KEY `dest_fund_FK_idx1` (`dest_fund_FK`),
+    CONSTRAINT `dest_fund_FK` FOREIGN KEY (`dest_fund_FK`) REFERENCES `department_lookup` (`id`),
+    CONSTRAINT `source_fund_FK` FOREIGN KEY (`source_fund_FK`) REFERENCES `department_lookup` (`id`)
 ) ENGINE = InnoDB
-  DEFAULT CHARACTER SET = utf8mb4
+  AUTO_INCREMENT = 23
+  DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_0900_ai_ci;
 
 
@@ -158,124 +156,124 @@ CREATE TABLE `meetdb`.`employee_card`
 
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To England', 1400, 'Travel', 'This is a travel plan', "2019-11-25 23:20:20", "2019-11-29 23:20:20", 1,
-        1, 1, 1);
+        1, 1, "Low", 1);
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To China', 1600, 'Travel', 'This is a travel plan', "2019-11-21 05:20:20", "2020-01-29 05:20:20", 2, 2,
-        1, 1);
+        1, "Low", 1);
 
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Taiwan', 2250, 'Travel', 'This is a travel plan', "2019-12-25 23:20:20", "2019-12-29 23:20:20", 1, 1,
-        1, 1);
+        1, "Medium", 1);
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To India', 3256, 'Travel', 'This is a travel plan', "2019-12-21 05:20:20", "2019-12-29 05:20:20", 2, 2,
-        1, 1);
+        1, "Low", 1);
 
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Dallas', 500, 'Travel', 'This is a travel plan', "2020-01-20 23:20:20", "2020-01-22 23:20:20", 7, 7,
-        1, 1);
+        1, "Low", 1);
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Los Angeles', 500, 'Travel', 'This is a travel plan', "2020-01-04 05:20:20", "2020-01-15 05:20:20",
-        3, 3, 1, 1);
+        3, 3, 1, "Low", 1);
 
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Nantucket', 500, 'Travel', 'This is a travel plan', "2020-01-20 23:20:20", "2020-01-22 23:20:20", 2,
         2,
-        1, 1);
+        1, "Medium", 1);
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To New Jersey', 2410, 'Travel', 'This is a travel plan', "2020-02-20 23:20:20", "2020-02-22 23:20:20",
         2,
         2,
-        1, 1);
+        1, "Low", 1);
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Austin', 500, 'Travel', 'This is a travel plan', "2020-01-04 05:20:20", "2020-01-15 05:20:20",
-        2, 2, 1, 1);
+        2, 2, 1, "Low", 1);
 
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To New York', 400, 'Travel', 'This is a travel plan', "2020-02-05 05:20:20", "2020-01-15 05:20:20", 5,
-        5, 1, 1);
+        5, 1, "Medium", 1);
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Atlanta', 800, 'Travel', 'This is a travel plan', "2020-02-15 05:20:20", "2020-02-29 05:20:20", 5, 5,
-        1, 1);
+        1, "Low", 1);
 
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Salt Lake City', 400, 'Travel', 'This is a travel plan', "2020-03-15 05:20:20",
-        "2020-03-29 05:20:20", 5, 5, 1, 1);
+        "2020-03-29 05:20:20", 5, 5, 1, "Low", 1);
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Chicago', 1235, 'Travel', 'This is a travel plan', "2020-03-20 05:20:20", "2020-03-25 05:20:20", 2,
-        2, 1, 1);
+        2, 1, "High", 1);
 
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Montgomery', 400, 'Travel', 'This is a travel plan', "2020-03-15 05:20:20",
-        "2020-03-29 05:20:20", 2, 2, 1, 1);
+        "2020-03-29 05:20:20", 2, 2, 1, "Low", 1);
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Miami', 2654, 'Travel', 'This is a travel plan', "2020-03-20 05:20:20", "2020-03-25 05:20:20", 2,
-        2, 1, 1);
+        2, 1, "Urgent", 1);
 
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Charleston', 400, 'Travel', 'This is a travel plan', "2020-03-16 05:20:20", "2020-03-29 05:20:20", 5,
-        5, 1, 1);
+        5, 1, "Urgent", 1);
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Indianapolis', 1235, 'Travel', 'This is a travel plan', "2020-03-16 05:20:20", "2020-03-25 05:20:20",
-        2, 2, 1, 1);
+        2, 2, 1, "Low", 1);
 
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Seattle', 1000, 'Travel', 'This is a travel plan', "2020-04-12 05:20:20", "2020-04-12 23:20:20", 4,
-        4, 1, 1);
+        4, 1, "Low", 1);
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Omaha', 1000, 'Travel', 'This is a travel plan', "2020-04-13 05:20:20", "2020-04-17 23:20:20", 4, 4,
-        1, 1);
+        1, "Low", 1);
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Phoenix', 1100, 'Travel', 'This is a travel plan', "2020-04-20 23:20:20", "2020-04-22 23:20:20", 2,
         2,
-        1, 1);
+        1, "Medium", 1);
 
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Tacoma', 1000, 'Travel', 'This is a travel plan', "2020-05-12 05:20:20", "2020-05-12 23:20:20", 1, 1,
-        1, 1);
+        1, "Low", 1);
 
 INSERT INTO plan(plan_name, funding_amount, plan_justification, memo, start_date, end_date, source_fund_FK,
-                 dest_fund_FK, fund_individuals, complete)
+                 dest_fund_FK, fund_individuals, priority, complete)
 VALUES ('Travel To Rio', 2541, 'Travel', 'This is a travel plan', "2020-05-13 05:20:20", "2020-05-17 23:20:20", 1, 1, 1,
-        1);
+        "Low", 1);
