e861b3f0b588afc9f25a42b7b2114688715120ab
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-28 01:26:42 -0400

    + Added InlineSubmitField with widget to use button element rather than input

--- modules/routes/utils/custom_fields.py
+++ modules/routes/utils/custom_fields.py
@@ -1,5 +1,5 @@
 from markupsafe import Markup
-from wtforms import BooleanField
+from wtforms import Field
 from wtforms.widgets.core import html_params
 
 
@@ -15,11 +15,18 @@ class InlineButtonWidget(object):
         kwargs.setdefault('type', self.input_type)
         if 'value' not in kwargs:
             kwargs['value'] = field._value()
-        return Markup(self.html % (self.html_params(name=field.name, **kwargs), "Login"))
+        return Markup(self.html % (self.html_params(name=field.name, **kwargs), field.btn_text))
 
 
-class InlineSubmitField(BooleanField):
-    def __init__(self, label='', validators=None, **kwargs):
-        super(InlineSubmitField, self).__init__(label, validators, **kwargs)
-
+class InlineSubmitField(Field):
     widget = InlineButtonWidget()
+
+    def __init__(self, label='', validators=None, btn_text='Submit', **kwargs):
+        super(InlineSubmitField, self).__init__(label, validators, **kwargs)
+        self.btn_text = btn_text
+
+    def _value(self):
+        if self.data:
+            return u''.join(self.data)
+        else:
+            return u''
