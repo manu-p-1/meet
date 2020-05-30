b72f53d3a6d142fcc3ca02af072bd2ad25a53c7a
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-29 23:20:52 -0400

    + Reformatting plus conversion to local timezone

--- modules/routes/utils/routes.py
+++ modules/routes/utils/routes.py
@@ -1,9 +1,12 @@
 import sys
+from datetime import datetime
+
+from dateutil import tz
 from flask import Blueprint, render_template, request, jsonify, session
 import json
 
 from modules.decorators.utils import login_required
-from modules.routes.utils.classes.class_utils import ManipulationType
+from modules.routes.utils.classes.class_utils import ManipulationType, SupportedTimeFormats
 from modules.routes.utils.functions.function_utils import is_active_plan, short_error, short_success
 from server import mysql
 
@@ -79,84 +82,97 @@ def manage_plan():
     cursor = conn.cursor()
 
     search_query = request.args.get('value')
+    timezone = request.args.get('tz')
+
+    if search_query is None or timezone is None:
+        return short_error(err_list=['An error retrieving the plan occured'])
 
     cursor.execute('SELECT * FROM PLAN where plan_name = %s', search_query)
     plan_data = cursor.fetchall()
 
-    if len(plan_data) != 0:
-        session['CURRENT_PLAN_EDIT'] = plan_data[0][0]
+    if len(plan_data) == 0:
+        return short_error(err_list=['A plan with this name could not be found.'])
 
-        headers = [desc[0] for desc in cursor.description]
-        plan = zip(headers, plan_data[0])
-        plan_fmt = {
-            table_headers: val for (table_headers, val) in plan
+    session['CURRENT_PLAN_EDIT'] = plan_data[0][0]
+
+    headers = [desc[0] for desc in cursor.description]
+    plan = zip(headers, plan_data[0])
+    plan_fmt = {
+        table_headers: val for (table_headers, val) in plan
+    }
+
+    cursor.execute('SELECT department FROM department_lookup WHERE id = %s', plan_fmt['dest_fund_FK'])
+
+    plan_fmt['dest_fund_FK'] = cursor.fetchall()[0][0]
+    plan_fmt['funding_amount'] = float("{:.2f}".format(float(plan_fmt['funding_amount'])))
+    plan_fmt['start_date'] = convert_time_zone(plan_fmt['start_date'], timezone)
+
+    if plan_fmt['amount_limit'] is not None:
+        plan_fmt['amount_limit'] = float("{:.2f}".format(float(plan_fmt['amount_limit'])))
+
+    if plan_fmt['end_date'] is not None:
+        plan_fmt['end_date'] = convert_time_zone(plan_fmt['end_date'], timezone)
+
+    plan_fmt['fund_individuals'] = True if int.from_bytes(plan_fmt['fund_individuals'], 'big') else False
+    plan_fmt['is_active'] = True if is_active_plan(mysql, plan_fmt['plan_name']) else False
+
+    employees_list = []
+    if plan_fmt['fund_individuals']:
+        get_employees_query = """
+        SELECT id, CONCAT(first_name, ' ', last_name) AS name FROM employee 
+        JOIN employee_plan ep on employee.id = ep.ep_employee_FK WHERE ep_plan_FK = %s
+        """
+        cursor.execute(get_employees_query, session['CURRENT_PLAN_EDIT'])
+        cf = cursor.fetchall()
+        for employee in cf:
+            employees_list.append({
+                "id": employee[0],
+                "name": employee[1]
+            })
+    plan_fmt['employees_list'] = employees_list
+
+    session['MANAGE_FORM'] = plan_fmt
+    conn.close()
+
+    return jsonify(
+        response_status="success",
+        active_status={
+            "status": plan_fmt['is_active'],
+            "info": None if not plan_fmt['is_active'] else render_template("plans/plan_info_partial.html",
+                                                                           info_message="This plan is already "
+                                                                                        "active "
+                                                                                        "and cannot be modified. "
+                                                                                        "It "
+                                                                                        "can only "
+                                                                                        "be deleted.")
+        },
+        response={
+            "plan_name": plan_fmt['plan_name'],
+            "funding_amount": plan_fmt['funding_amount'],
+            "justification": plan_fmt['plan_justification'],
+            "memo": plan_fmt['memo'],
+            "start_date": plan_fmt['start_date'],
+            "dest_fund": plan_fmt['dest_fund_FK'],
+            "has_employee_specific": True if plan_fmt['fund_individuals'] else False,
+            "employees_list": plan_fmt['employees_list'],
+            "has_end_date": True if plan_fmt['end_date'] is not None else False,
+            "end_date": plan_fmt['end_date'],
+            "has_velocity_control": True if plan_fmt['control_name'] is not None else False,
+            "control_name": plan_fmt['control_name'],
+            "control_window": plan_fmt['control_window'],
+            "amount_limit": plan_fmt['amount_limit'],
+            "usage_limit": plan_fmt['usage_limit'],
+            "priority": plan_fmt['priority']
         }
+    )
 
-        cursor.execute('SELECT department FROM department_lookup WHERE id = %s', plan_fmt['dest_fund_FK'])
 
-        plan_fmt['dest_fund_FK'] = cursor.fetchall()[0][0]
-        plan_fmt['funding_amount'] = float("{:.2f}".format(float(plan_fmt['funding_amount'])))
-        plan_fmt['start_date'] = plan_fmt['start_date'].strftime("%Y-%m-%d %H:%M")
-
-        if plan_fmt['amount_limit'] is not None:
-            plan_fmt['amount_limit'] = float("{:.2f}".format(float(plan_fmt['amount_limit'])))
-
-        if plan_fmt['end_date'] is not None:
-            plan_fmt['end_date'] = plan_fmt['end_date'].strftime("%Y-%m-%d %H:%M")
-
-        plan_fmt['fund_individuals'] = True if int.from_bytes(plan_fmt['fund_individuals'], 'big') else False
-        plan_fmt['is_active'] = True if is_active_plan(mysql, plan_fmt['plan_name']) else False
-
-        employees_list = []
-        if plan_fmt['fund_individuals']:
-            get_employees_query = """
-            SELECT id, CONCAT(first_name, ' ', last_name) AS name FROM employee 
-            JOIN employee_plan ep on employee.id = ep.ep_employee_FK WHERE ep_plan_FK = %s
-            """
-            cursor.execute(get_employees_query, session['CURRENT_PLAN_EDIT'])
-            cf = cursor.fetchall()
-            for employee in cf:
-                employees_list.append({
-                    "id": employee[0],
-                    "name": employee[1]
-                })
-        plan_fmt['employees_list'] = employees_list
-
-        session['MANAGE_FORM'] = plan_fmt
-        conn.close()
-
-        return jsonify(
-            response_status="success",
-            active_status={
-                "status": plan_fmt['is_active'],
-                "info": None if not plan_fmt['is_active'] else render_template("plans/plan_info_partial.html",
-                                                                               info_message="This plan is already "
-                                                                                            "active "
-                                                                                            "and cannot be modified. "
-                                                                                            "It "
-                                                                                            "can only "
-                                                                                            "be deleted.")
-            },
-            response={
-                "plan_name": plan_fmt['plan_name'],
-                "funding_amount": plan_fmt['funding_amount'],
-                "justification": plan_fmt['plan_justification'],
-                "memo": plan_fmt['memo'],
-                "start_date": plan_fmt['start_date'],
-                "dest_fund": plan_fmt['dest_fund_FK'],
-                "has_employee_specific": True if plan_fmt['fund_individuals'] else False,
-                "employees_list": plan_fmt['employees_list'],
-                "has_end_date": True if plan_fmt['end_date'] is not None else False,
-                "end_date": plan_fmt['end_date'],
-                "has_velocity_control": True if plan_fmt['control_name'] is not None else False,
-                "control_name": plan_fmt['control_name'],
-                "control_window": plan_fmt['control_window'],
-                "amount_limit": plan_fmt['amount_limit'],
-                "usage_limit": plan_fmt['usage_limit'],
-                "priority": plan_fmt['priority']
-            }
-        )
-    return short_error(err_list=['A plan with this name could not be found.'])
+def convert_time_zone(date_time_obj: datetime, timezone):
+    print(datetime, timezone, file=sys.stderr)
+    from_zone = tz.gettz('UTC')
+    to_zone = tz.gettz(timezone)
+    dt_local = date_time_obj.replace(tzinfo=from_zone).astimezone(to_zone)
+    return dt_local.strftime(SupportedTimeFormats.FMT_UI)
 
 
 @util_bp.route('plans/find/delete_plan/', methods=['POST'])
