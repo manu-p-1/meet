de7505f93b4600d196eb065e5199f667effe42e1
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-05-29 23:12:57 -0400

    + Added supported date format strings

--- modules/routes/utils/classes/class_utils.py
+++ modules/routes/utils/classes/class_utils.py
@@ -10,3 +10,8 @@ class ManipulationType(enum.Enum):
 class OperationType(enum.Enum):
     ADD_EMPLOYEE = 'add'
     DELETE_EMPLOYEE = 'del'
+
+
+class SupportedTimeFormats:
+    FMT_UTC = "%Y-%m-%d %H:%M:%S"
+    FMT_UI = "%m/%d/%Y %I:%M %p"
