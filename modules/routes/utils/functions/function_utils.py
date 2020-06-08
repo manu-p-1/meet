8a6f779286060f256a456a32afa664628a2272f8
Manu Puduvalli <28782438+manu-p-1@users.noreply.github.com>
2020-06-07 20:25:20 -0400

    Reformatting and added expired plan logic func

--- modules/routes/utils/functions/function_utils.py
+++ modules/routes/utils/functions/function_utils.py
@@ -4,7 +4,7 @@ from typing import Union, Dict, List, Any
 from flask import render_template, jsonify
 
 from modules.routes.user.custom_fields import ISimpleEmployee
-from modules.routes.utils.classes.class_utils import ManipulationType
+from modules.routes.utils.classes.class_utils import ManipulationType, SupportedTimeFormats
 
 
 def gather_form_errors(form):
@@ -32,15 +32,34 @@ def is_active_plan(mysql, field_data) -> bool:
     conn = mysql.connect()
     cursor = conn.cursor()
     now = datetime.now(timezone.utc)
-    start_date = now.strftime("%Y-%m-%d %H:%M:%S")
+    start_date = now.strftime(SupportedTimeFormats.FMT_UTC)
     q = '''SELECT plan_name FROM plan WHERE plan_name = %s AND start_date < %s'''
     cursor.execute(q, (field_data, start_date))
-    if len(cursor.fetchall()) == 0:
-        conn.close()
+    fetch = cursor.fetchall()
+    conn.close()
+
+    return False if len(fetch) == 0 else True
+
+
+def is_expired_plan(mysql, plan_name) -> bool:
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    now = datetime.now(timezone.utc)
+
+    q = """SELECT end_date FROM plan WHERE plan_name = %s"""
+    cursor.execute(q, plan_name)
+    fetch = cursor.fetchall()
+    conn.close()
+
+    if len(fetch) == 0:
+        raise Exception("Plan name could not be found")
+
+    if fetch[0][0] is None:
         return False
-    else:
-        conn.close()
-        return True
+
+    end_date_obj = datetime.strptime(fetch[0][0], SupportedTimeFormats.FMT_UTC)
+
+    return True if now > end_date_obj else False
 
 
 def find_all_employees(cursor, dept) -> Union[Dict[str, Union[List[ISimpleEmployee], int]], Any]:
@@ -51,6 +70,7 @@ def find_all_employees(cursor, dept) -> Union[Dict[str, Union[List[ISimpleEmploy
     """
     cursor.execute(q, dept)
     employees = cursor.fetchall()
+
     len_emps = len(employees)
     if employees is not None and len_emps != 0:
         return {
